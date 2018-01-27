98d8ab08c5b1aecd79e3ec001e76226199a2defd
SimenLK <simenlundkirkvik@hotmail.com>
2018-01-27 13:56:38 +0100

    Improved install script to go out of current directory

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Install script for conveniently taking out the report template and removing itself 
-
-cp report.tex pre.tex ../ && cd ../ && rm -rf latexreport
+# cdout is a alias (cd ..)
+cp report.tex pre.tex ../ && cdout && rm -rf latexreport
