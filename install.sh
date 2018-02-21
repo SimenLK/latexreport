e9bc646cc3b1615c00e2a5c0abf21eb281993692
SimenLK <Simenlundkirkvik@hotmail.com>
2018-02-21 12:24:15 +0100

    Trying something to install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,14 @@
 
 mv report.tex pre.tex ../ 
 echo "Moving the files out of the directory"
+
 cd .. 
+echo "Moving out of the directroy"
+
 rm -rf latexreport
 echo "Deleting the directory"
+
+if [ -d latexreport ]
+then
+	echo "Latexreport is still there!"
+fi
