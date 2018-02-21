4e52467c1833b3429913a1acb1d1775d19da7fe1
SimenLK <Simenlundkirkvik@hotmail.com>
2018-02-21 12:35:20 +0100

    Add test for checking if dir is there
    Removing readme before deleting

--- install.sh
+++ install.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 # Install script for conveniently taking out the report template and removing itself 
 
+dirname = ${PWD##*/}
+
+rm README.md
+echo "Removing README.md"
+
 mv report.tex pre.tex ../ 
 echo "Moving the files out of the directory"
 
 cd .. 
 echo "Moving out of the directroy"
 
-rm -rf latexreport
+rm -rf $dirname
 echo "Deleting the directory"
 
-if [ -d latexreport ]
+if [ -d $dirname ]
 then
 	echo "Latexreport is still there!"
 fi
