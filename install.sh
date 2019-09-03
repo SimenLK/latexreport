ba42838175225d3517ff879b5009f74f7bce9ae2
SimenLK <simenlundkirkvik@hotmail.com>
2019-09-03 21:36:13 +0200

    Added script to move references.bib to home documents

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 # Install script for conveniently taking out the report template and removing itself 
 
 DIR="latexreport"
+# Change this to set the
+REF=~/Documents/references.bib
+REF_DIR=~/Documents/
 
 mv $DIR/report.tex $DIR/pre.tex $DIR/references.bib .
 echo "Moving the files out of $DIR"
@@ -14,4 +17,12 @@ then
 	rm -rf "$DIR"
 else
 	echo "$DIR was succesfully removed!"
+fi
+
+if [ -f "$REF" ]; then
+    echo "References already there"
+    rm "$REF"
+else
+    echo "Moving references to $REF_DIR"
+    mv references.bib $REF_DIR
 fi
