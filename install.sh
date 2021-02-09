48ad8e620a36b3349807988349c49a120888c78e
Simen Kirkvik <simenlundkirkvik@hotmail.com>
2021-02-09 15:31:12 +0100

    Do not need to save references.bib

--- install.sh
+++ install.sh
@@ -3,26 +3,16 @@
 
 DIR="latexreport"
 # Change this to set the
-REF=~/Documents/references.bib
-REF_DIR=~/Documents/
+REF=~/keybase/public/simenlk/references.bib
+REF_DIR=~/keybase/public/simenlk/
 
 mv $DIR/report.tex $DIR/pre.tex $DIR/references.bib .
 echo "Moving the files out of $DIR"
 
-rm -rf "$DIR"
-echo "Deleting the directory"
-
 if [ -d "$DIR" ]; 
 then
-	rm -rf "$DIR"
+    rm -rf "$DIR"
 else
-	echo "$DIR was succesfully removed!"
+    echo "$DIR was succesfully removed!"
 fi
 
-if [ -f "$REF" ]; then
-    echo "References already there"
-    rm references.bib
-else
-    echo "Moving references to $REF_DIR"
-    mv references.bib $REF_DIR
-fi
