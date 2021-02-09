7f65f2d4ea7e1c84adeac844dded573fff5fb18f
Simen Kirkvik <simenlundkirkvik@hotmail.com>
2021-02-09 16:16:42 +0100

    Further remove references.tex and exit(0)

--- install.sh
+++ install.sh
@@ -2,17 +2,16 @@
 # Install script for conveniently taking out the report template and removing itself 
 
 DIR="latexreport"
-# Change this to set the
-REF=~/keybase/public/simenlk/references.bib
-REF_DIR=~/keybase/public/simenlk/
 
-mv $DIR/report.tex $DIR/pre.tex $DIR/references.bib .
+mv $DIR/report.tex $DIR/pre.tex .
 echo "Moving the files out of $DIR"
 
 if [ -d "$DIR" ]; 
 then
     rm -rf "$DIR"
-else
     echo "$DIR was succesfully removed!"
+else
+    echo "Error"
 fi
 
+exit(0)
